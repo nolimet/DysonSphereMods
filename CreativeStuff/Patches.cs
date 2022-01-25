0b1a5f120e748c68ebe7cffc502accf77f0ccbd1
Jesse Stam <nolimet@stamdata.nl>
2022-01-25 21:03:57 +0100

    Cleaned up files and allowed for multi output

--- CreativeStuff/Patches.cs
+++ CreativeStuff/Patches.cs
@@ -1,5 +1,4 @@
 ﻿using HarmonyLib;
-using BepInEx.Logging;
 
 namespace TestMod
 {
@@ -9,14 +8,25 @@ namespace TestMod
         [HarmonyPatch(typeof(CargoTraffic), "UpdateSplitter")]
         public static bool CargoTraffic_UpdateSplitter_Prefix(CargoTraffic __instance, int id, int input0, int input1, int input2, int output0, int output1, int output2, int filter)
         {
-            if (input0 == 0 && output0 != 0 && filter != 0)
+            if (filter != 0 && input0 == 0 && input1 == 0 && input2 == 0)
             {
-                var outputPath = __instance.GetCargoPath(__instance.beltPool[output0].segPathId);
-                if (outputPath != null && outputPath.TestBlankAtHead() == 0)
+                OutputForOutput(output0);
+                OutputForOutput(output1);
+                OutputForOutput(output2);
+            }
+
+            void OutputForOutput(int output)
+            {
+                if (output != 0)
                 {
-                    outputPath.TryInsertItemAtHead(filter, 1, 1);
+                    var outputPath = __instance.GetCargoPath(__instance.beltPool[output].segPathId);
+                    if (outputPath != null && outputPath.TestBlankAtHead() == 0)
+                    {
+                        outputPath.TryInsertItemAtHead(filter, 1, 1);
+                    }
                 }
             }
+
             return true;
         }
     }
--- CreativeStuff/Plugin.cs
+++ CreativeStuff/Plugin.cs
@@ -1,6 +1,5 @@
 ﻿using BepInEx;
 using HarmonyLib;
-using System.Threading.Tasks;
 
 namespace TestMod
 {
