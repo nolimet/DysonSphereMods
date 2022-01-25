16efa48eaa055bf936c855fcf9c55123ceb71991
Jesse Stam <nolimet@stamdata.nl>
2022-01-25 20:33:14 +0100

    Added super large mod

---
+++ CreativeStuff/Patches.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using BepInEx.Logging;
+
+namespace TestMod
+{
+    public static class Patches
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(CargoTraffic), "UpdateSplitter")]
+        public static bool CargoTraffic_UpdateSplitter_Prefix(CargoTraffic __instance, int id, int input0, int input1, int input2, int output0, int output1, int output2, int filter)
+        {
+            if (input0 == 0 && output0 != 0 && filter != 0)
+            {
+                var outputPath = __instance.GetCargoPath(__instance.beltPool[output0].segPathId);
+                if (outputPath != null && outputPath.TestBlankAtHead() == 0)
+                {
+                    outputPath.TryInsertItemAtHead(filter, 1, 1);
+                }
+            }
+            return true;
+        }
+    }
+}
